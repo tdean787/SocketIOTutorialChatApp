fd66a854a17950876bb1500878f0281713506a4b
Taylor Dean <taylordean787@gmail.com>
2021-03-18 11:24:38 -0500

    Adding user tracking functionality

--- index.js
+++ index.js
@@ -9,28 +9,40 @@ http.listen(3001, () => {
   console.log("listening on 3001");
 });
 
+let users = [];
+
 io.on("connection", (socket) => {
   console.log("user connected");
-
-  console.log("ip: " + socket.request.connection.remoteAddress);
-  console.log("user-agent: " + socket.request.headers["user-agent"]);
+  // console.log("ip: " + socket.request.connection.remoteAddress);
+  // console.log("user-agent: " + socket.request.headers["user-agent"]);
   // socket.on("disconnect", () => {
   //   console.log("disconnected");
   // });
+
+  socket.on("setUsername", (data) => {
+    if (users.indexOf(data) > -1) {
+      users.push(data);
+      socket.emit("userSet", { username: data });
+    } else {
+      socket.emit(
+        "userExists",
+        data + " username is taken. Please try another."
+      );
+    }
+  });
+
+  socket.on("typing", (data) => {
+    // console.log("typing registered");
+    io.emit("typing", data);
+  });
+
   socket.on("chat message", (msg) => {
     console.log("message: " + msg);
   });
 
-  socket.on("typingMessage");
-});
-
-io.emit("some event", {
-  someProperty: "some value",
-  otherProperty: "other value",
-});
-
-io.on("connection", (socket) => {
+  // io.on("connection", (socket) => {
   socket.on("chat message", (msg) => {
     io.emit("chat message", msg);
   });
+  // });
 });
