ce6e88118df637b269070b2d544ac6a962215524
Taylor Dean <taylordean787@gmail.com>
2021-03-18 09:07:22 -0500

    Adding typing user functionality. Restucturing file directory

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 const app = require("express")();
 const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const express = require("express");
+// app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
+app.use(express.static("public"));
 
-app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
+http.listen(3001, () => {
+  console.log("listening on 3001");
+});
 
-http.listen(3000, () => {
-  console.log("listening on 3000");
+io.on("connection", (socket) => {
+  console.log("user connected");
+
+  console.log("ip: " + socket.request.connection.remoteAddress);
+  console.log("user-agent: " + socket.request.headers["user-agent"]);
+  // socket.on("disconnect", () => {
+  //   console.log("disconnected");
+  // });
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+  });
+
+  socket.on("typingMessage");
+});
+
+io.emit("some event", {
+  someProperty: "some value",
+  otherProperty: "other value",
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
 });
